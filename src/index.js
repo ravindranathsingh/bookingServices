695148d27beea360732bc1935030d44b5cb197f8
Ravindranath Singh <ravindranathsingh391@gmail.com>
2024-03-12 02:00:22 +0530

    Added basic routes function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
 const app = express();
 
 const { PORT } = require('./config/server-config')
+const apiRoutes = require('./routes/index')
+const db = require('./models/index')
 
 const serverSetUp = () => {
 
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({extended: true}))
 
+    app.use('/api', apiRoutes)
+
     app.listen(PORT, async () => {
         console.log(`Server started at port ${PORT}`)
+        if(process.env.DB_SYNC) {
+            db.sequelize.sync({alter: true})
+        }
     })
 }
 
