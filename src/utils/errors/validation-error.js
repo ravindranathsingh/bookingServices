0adbb65cb5f729c30dcd77519be8d0f3e2bb7411
Ravindranath Singh <ravindranathsingh391@gmail.com>
2024-03-12 23:51:10 +0530

    Minor fix on error functions

--- src/utils/errors/validation-error.js
+++ src/utils/errors/validation-error.js
@@ -1,14 +1,16 @@
 const { StatusCodes } = require('http-status-codes')
 
 class ValidationError extends Error {
-    constructor(
-        explanation = [],
-        statuscode = StatusCodes.BAD_REQUEST
-    ) {
+    constructor(error) {
+        super();
+        let explanation = []
+        error.errors.forEach((err) => {
+            explanation.push(err.message)
+        })
         this.name = 'Validation Error',
         this.message = 'Not able to validate the data sent into the request',
         this.explanation = explanation,
-        this.statuscode = statuscode
+        this.statuscode = StatusCodes.BAD_REQUEST
     }
 }
 
