2ce7c84d42de68826f79801d9c01764fe502dc71
Ravindranath Singh <ravindranathsingh391@gmail.com>
2024-03-15 08:14:36 +0530

    Fixed the route error

--- src/service/booking-service.js
+++ src/service/booking-service.js
@@ -16,17 +16,17 @@ class BookingServices {
             let getFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`;
             const response = await axios.get(getFlightRequestURL);
             const flightData = response.data.data;
-            const priceOfTheFlight = flightData.price;
-            if (data.noOfSeats > flightData.totalSeats) {
-                throw new ServiceError(
-                    'Something went wrong in booking process', 
-                    'Insufficient Seats in the Flight'
-                )
-            }
-            const totalCost = priceOfTheFlight * data.noOfSeats;
-            const bookingPayLoad = {...data, totalCost}
-            const booking = await this.bookingRepository.create(bookingPayLoad)
-            return booking;
+            // const priceOfTheFlight = flightData.price;
+            // if (data.noOfSeats > flightData.totalSeats) {
+            //     throw new ServiceError(
+            //         'Something went wrong in booking process', 
+            //         'Insufficient Seats in the Flight'
+            //     )
+            // }
+            // const totalCost = priceOfTheFlight * data.noOfSeats;
+            // const bookingPayLoad = {...data, totalCost}
+            // const booking = await this.bookingRepository.create(data)
+            return flightData;
         } catch (error) {
             if(error.name == 'RepositoryError' || error.name == 'ValidationError') {
                 throw error;
